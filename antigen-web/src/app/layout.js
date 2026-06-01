116abddc7d728334cd3276994afeee25685ebed2
s1ds <sid.mailbox@gmail.com>
2026-06-01 23:55:35 +0530

    Refactor metadata export in layout.js
    Removed type annotation for metadata export.

--- antigen-web/src/app/layout.js
+++ antigen-web/src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import { SiteFooter } from "@/components/site/SiteFooter";
 import { SiteHeader } from "@/components/site/SiteHeader";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'ANTIGEN',
   description: 'A post-algorithm organism built to tackle stagnation and sameness.',
   openGraph: {
